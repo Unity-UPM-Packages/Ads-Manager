aee8ac43d613ea04b49f49418a5fef092d40695f
QuangHaiVu <vuquanghai0108@gmail.com>
2025-10-27 11:56:49 +0700

    fix: use const string name for native admob layout

---
+++ ads.manager/Packages/com.thelegends.ads.manager/Runtime/AdsManager/Scripts/Enums/NativeName.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TheLegends.Base.Ads
+{
+    public static class NativeName
+    {
+        public const string Native_Banner = "native_banner";
+        public const string Native_Inter = "native_inter";
+        public const string Native_Mrec = "native_mrec";
+        public const string Native_Video = "native_video";
+    }
+}
